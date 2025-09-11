5f2083453c67543d2faa0bf950addbdaa7daf811
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-11 23:41:57 +0900

    feat: OpenAiConfig 추가

---
+++ src/main/java/com/dawn/cs_study/ai/infrastructure/ai/OpenAiConfig.java
@@ -0,0 +1,23 @@
+package com.dawn.cs_study.ai.infrastructure.ai;
+
+import org.springframework.ai.openai.api.OpenAiApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAiConfig {
+
+    @Value("${spring.ai.openai.api-key}")
+    private String apiKey;
+
+    @Bean
+    public OpenAiApi openAiApi() {
+        System.out.println(apiKey);
+        return OpenAiApi.builder()
+                .apiKey(apiKey)
+                .build();
+    }
+
+
+}
