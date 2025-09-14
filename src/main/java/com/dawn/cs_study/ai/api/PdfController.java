3724c8320d3ef268d1682ee0e156e04dd647fe3f
masiljangajji <xmfpdlsj0508@gmail.com>
2025-09-14 21:17:27 +0900

    refactor: Pdf 저장 로직 변경 (메모리 적재 관련 최적화 중)

--- src/main/java/com/dawn/cs_study/ai/api/PdfController.java
+++ src/main/java/com/dawn/cs_study/ai/api/PdfController.java
@@ -1,31 +1,54 @@
 package com.dawn.cs_study.ai.api;
 
 import com.dawn.cs_study.ai.api.response.ApiResponse;
+import com.dawn.cs_study.ai.application.EmbedPdfDisUseCase;
 import com.dawn.cs_study.ai.application.EmbedPdfUseCase;
 import com.dawn.cs_study.ai.domain.DocumentUploadResult;
 import com.dawn.cs_study.ai.domain.support.PdfValidator;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Objects;
-
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class PdfController {
 
     private final EmbedPdfUseCase embedPdfUseCase;
 
+    private final EmbedPdfDisUseCase embedPdfDisUseCase;
+
+    @GetMapping("/happy")
+    public String gg() {
+        return "good";
+    }
+
+
     @PostMapping(value = "/documents", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<ApiResponse<DocumentUploadResult>> uploadDocument(@RequestParam("file") MultipartFile file) {
 
-        PdfValidator.parsePdf(file);
-        String id = embedPdfUseCase.embedPdf(file);
+         String id = embedPdfUseCase.embedPdf(file);
+
+        return ResponseEntity.ok()
+                .body(
+                        new ApiResponse<>(
+                                new DocumentUploadResult(id, "문서 업로드 완료")
+                        )
+                );
+    }
+
+    // todo 성능 비교 및 최적화 과정 진행 중
+
+    @PostMapping(value = "/documents2", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<ApiResponse<DocumentUploadResult>> aa(@RequestParam("file") MultipartFile file) {
+        String id = embedPdfDisUseCase.embedPdf(file);
 
         return ResponseEntity.ok()
                 .body(
--- src/main/java/com/dawn/cs_study/ai/application/EmbedPdfUseCase.java
+++ src/main/java/com/dawn/cs_study/ai/application/EmbedPdfUseCase.java
@@ -1,23 +1,21 @@
-package com.dawn.cs_study.ai.application;// PdfIngestService.java
+package com.dawn.cs_study.ai.application;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-
-import com.dawn.cs_study.ai.application.port.ReadPdfPort;
 import com.dawn.cs_study.ai.application.port.VectorCommandPort;
+import com.dawn.cs_study.ai.domain.support.PdfValidator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.FileSystemResource;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
-import org.springframework.ai.reader.pdf.ParagraphPdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Service
@@ -26,65 +24,62 @@ public class EmbedPdfUseCase {
 
     private final VectorCommandPort vectorCommandPort;
 
-    private final ReadPdfPort readPdfPort;
-
     /**
-     * PDF 업로드를 받아 pgvector에 저장
+     * PDF 업로드를 받아 원본 저장 없이 pgvector에 저장
      */
-    // todo 개인 경로를, meta data 로 저장, 파일 저장 로직 고민하기
     public String embedPdf(MultipartFile file) {
-        String id = UUID.randomUUID().toString();
 
-        File tempFile = null;
-        Path temp = null;
+        // PdfValidator.validate(file);
+
+        final String id = UUID.randomUUID().toString();
+        final String original = file.getOriginalFilename();
+
+        ByteArrayResource resource = convertToByteArrayResource(file, original);
+        List<Document> chunks = convertToDocuments(resource, id, original);
+        vectorCommandPort.embedDocuments(chunks);
+        return id;
+    }
+
+    private ByteArrayResource convertToByteArrayResource(MultipartFile file, String original) {
 
         try {
-            // 0) 임시 파일 저장
-
-            String original = Objects.requireNonNullElse(file.getOriginalFilename(), "upload.pdf");
-
-            String uploadDir = "/Users/seungjae/Desktop/fs";
-
-            temp = Files.createTempFile(Path.of(uploadDir), "upload_", original);
-            tempFile = temp.toFile();
-
-            // 저장
-            file.transferTo(tempFile);
-
-            // 1) PDF → Document (문단→페이지 폴백)
-            Map<String, Object> baseMeta = Map.of(
-                    "id", id,
-                    "filename", original
-            );
-            List<Document> docs = readPdfPort.readPdfAsDocuments(temp, baseMeta);
-
-            // 2) Split (토큰 단위)
-            var splitter = TokenTextSplitter.builder()
-                    .withChunkSize(900)
-                    .withMinChunkSizeChars(400)
-                    .withMinChunkLengthToEmbed(20)
-                    .withKeepSeparator(true)
-                    .withMaxNumChunks(20_000)
-                    .build();
-
-            List<Document> chunks = new ArrayList<>();
-            for (Document d : docs) {
-                chunks.addAll(splitter.split(d));
-            }
-
-            // 3) VectorStore 저장
-            vectorCommandPort.add(chunks);
-
-            return id;
+            return new ByteArrayResource(file.getBytes()) {
+                @Override
+                public String getFilename() {
+                    return original;
+                }
+            };
         } catch (Exception e) {
-            throw new RuntimeException("PDF ingest 실패: " + e.getMessage(), e);
-        } finally {
-            if (tempFile != null) try {
-                Files.deleteIfExists(temp);
-            } catch (IOException ignore) {
-                log.error("temp file delete error: {}", tempFile.getAbsolutePath());
-            }
+            log.error("ByteArray 변환 실패 {}", e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
+    private List<Document> convertToDocuments(ByteArrayResource resource, String id, String original) {
+
+
+        // PDF → Document (페이지 단위)
+        Map<String, Object> baseMeta = Map.of("id", id, "filename", original);
+        PdfDocumentReaderConfig config = PdfDocumentReaderConfig.builder()
+                .withPagesPerDocument(1)
+                .build();
+
+        PagePdfDocumentReader reader = new PagePdfDocumentReader(resource, config);
+
+        List<Document> docs = reader.read();
+        for (Document d : docs) {
+            d.getMetadata().putAll(baseMeta);
+        }
+
+        // 토큰 단위 Split
+        TokenTextSplitter splitter = TokenTextSplitter.builder()
+                .withChunkSize(900)
+                .withMinChunkSizeChars(400)
+                .withMinChunkLengthToEmbed(20)
+                .withKeepSeparator(true)
+                .withMaxNumChunks(20_000)
+                .build();
+
+        return splitter.apply(docs);
+    }
 }
